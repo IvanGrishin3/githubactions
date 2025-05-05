b76673f3fc2c2da6f2a89a985d4a76848e29a330
Ivan Grishin <ivan.grishin@innowise.com>
2025-05-05 16:00:50 +0200

    fix pipeline

--- variable.tf
+++ variable.tf
@@ -2,14 +2,3 @@ variable "aws_region" {
   type    = string
   default = "eu-central-1"
 }
-
-variable "environment" {
-  type        = string
-  description = "Deployment environment"
-  default     = "staging"
-}
-
-variable "aws_infra_account_id" {
-  type    = string
-  default = "914797210606"
-}
